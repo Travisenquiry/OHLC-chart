82c67255a8d3423281f8e2a25bbd6017e9df46d7
travisenquiry <travisenquiry@gmail.com>
2020-08-21 16:25:08 +0800

    Applied useEffect() on ChartComponent

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ChartComponent = () => {
+    useEffect(() => {
+        let chart = document.getElementById("chart");
+        let ctx = chart.getContext("2d");
+        ctx.moveTo(0, 0);
+        ctx.lineTo(200, 100);
+        ctx.stroke();
+    });
+
     return (
         <canvas id="chart"></canvas>
     )
