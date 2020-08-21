9cf834180fd182fc847764872bcc007beb3d0532
travisenquiry <travisenquiry@gmail.com>
2020-08-21 18:18:11 +0800

    Changed chart to fit canvas into parent div. Added CSS folder and Main.css

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
 const ChartComponent = () => {
+
     useEffect(() => {
+        //Implemented chart canvas
         let chart = document.getElementById("chart");
         let ctx = chart.getContext("2d");
+        let chartDiv = document.getElementById("chart-div");
+
+        //Set canvas size to be chart-div size
+        //Attempted to use lifecycle to set size dynamically but met with a bug of maximum update depth
+        chart.width = chartDiv.clientWidth;
+        chart.height = chartDiv.clientHeight;
+
         ctx.moveTo(0, 0);
-        ctx.lineTo(200, 100);
+        ctx.lineTo(50, 500);
         ctx.stroke();
     });
 
     return (
-        <canvas id="chart"></canvas>
+        <div id="chart-div">
+            <canvas id="chart"></canvas>
+        </div>
     )
 };
 
