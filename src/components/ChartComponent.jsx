879683050ae2d5291f87b8a891266a0dd30504f1
travisenquiry <travisenquiry@gmail.com>
2020-08-22 16:35:54 +0800

    Barebone base chart completed.

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -12,10 +12,23 @@ const ChartComponent = () => {
         //Attempted to use lifecycle to set size dynamically but met with a bug of maximum update depth
         chart.width = 900;
         chart.height = 400;
-
+        
+        //Code for chart background (Most outer X & Y line)
+        ctx.beginPath();
         ctx.moveTo(50, 0);
         ctx.lineTo(50, 400);
+        ctx.lineTo(850, 400);
         ctx.stroke();
+        
+        //Loop for individual grey lines (Total 50 results)
+        for(let i=1; i<51; i++){
+            let nextLine = i*16
+            ctx.beginPath();
+            ctx.strokeStyle = 'grey';
+            ctx.moveTo(50 + nextLine, 0);
+            ctx.lineTo(50 + nextLine, 400);
+            ctx.stroke();
+        }
     });
 
     return (
