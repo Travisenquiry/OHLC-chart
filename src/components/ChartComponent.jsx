9d4b55d765785c28f25701dcc62c5636946c3b9c
travisenquiry <travisenquiry@gmail.com>
2020-08-22 17:07:03 +0800

    Working test of symbols with hardcoded data

--- src/components/ChartComponent.jsx
+++ src/components/ChartComponent.jsx
@@ -24,11 +24,47 @@ const ChartComponent = () => {
         for(let i=1; i<51; i++){
             let nextLine = i*16
             ctx.beginPath();
+            ctx.lineWidth = 0.5
             ctx.strokeStyle = 'grey';
             ctx.moveTo(50 + nextLine, 0);
             ctx.lineTo(50 + nextLine, 400);
             ctx.stroke();
         }
+
+        //Testing of symbols (bullish)
+        ctx.beginPath();
+        ctx.strokeStyle = '#58bf66';
+        ctx.lineWidth = 3;
+        //High-to-low
+        ctx.moveTo(66, 50);
+        ctx.lineTo(66, 90);
+        ctx.stroke();
+        //Open
+        ctx.moveTo(66, 80);
+        ctx.lineTo(59, 80);
+        ctx.stroke();
+        //Close
+        ctx.moveTo(66, 57);
+        ctx.lineTo(73, 57);
+        ctx.stroke();
+
+        //Testing of symbols (bearish)
+        ctx.beginPath();
+        ctx.strokeStyle = '#ee6f72';
+        ctx.lineWidth = 3;
+        //High-to-low
+        ctx.moveTo(82, 50);
+        ctx.lineTo(82, 80);
+        ctx.stroke();
+        //Open
+        ctx.moveTo(82, 56);
+        ctx.lineTo(75, 56);
+        ctx.stroke();
+        //Close
+        ctx.moveTo(82, 59);
+        ctx.lineTo(89, 59);
+        ctx.stroke();
+
     });
 
     return (
