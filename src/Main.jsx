ddff13269d55c829a1f565f07f266b2fe784f9fe
travisenquiry <travisenquiry@gmail.com>
2020-08-24 17:22:11 +0800

    Added mouseover and mouseout event for buttons

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ChartComponent from './components/Stock/Ticker/Chart/ChartComponent.jsx';
 import ButtonComponent from './components/Stock/Ticker/Button/ButtonComponent.jsx';
 import './Main.css';
@@ -6,6 +6,26 @@ import './Main.css';
 const Main = () => {
     //State used for symbol
     const [stockSymbol, setStockSymbol] = useState('');
+
+    useEffect(() => {
+        //Add event listeners to all ticker button
+        let tickerButtons = document.getElementsByClassName("ticker");
+        for (let i=0; i < tickerButtons.length; i++) {
+            //Add mouseover event to change button color on hover
+            tickerButtons[i].addEventListener("mouseover", (() => {
+                tickerButtons[i].classList.add("selected");
+                tickerButtons[i].classList.remove("unselected");
+            }));
+
+            //Add mouseout event to change button color on leaving
+            tickerButtons[i].addEventListener("mouseout", (() => {
+                tickerButtons[i].classList.add("unselected");
+                tickerButtons[i].classList.remove("selected");
+            }));
+        }
+
+
+    }, []);
 
     return (
         <div className="container">
--- src/components/Stock/Ticker/Button/ButtonComponent.jsx
+++ src/components/Stock/Ticker/Button/ButtonComponent.jsx
@@ -2,7 +2,17 @@ import React, { useEffect } from 'react';
 import './ButtonComponent.css';
 
 const ButtonComponent = () => {
-    //
+
+    useEffect(() => {
+
+        //Add event listeners to all ticker button
+        let tickerButton = document.getElementsByClassName("ticker")[1];
+        tickerButton.addEventListener("mouseover", (() => {
+            tickerButton.classList.add("selected");
+            tickerButton.classList.remove("unselected");
+        }));
+
+    }, []);
 
     return (
         <div className="button-div">
