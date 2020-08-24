cf31537b27decf70c9b932e473ec22c713b8f045
travisenquiry <travisenquiry@gmail.com>
2020-08-24 22:11:18 +0800

    Added css check on button component for selected and unselected

--- src/components/Stock/Ticker/Button/ButtonComponent.jsx
+++ src/components/Stock/Ticker/Button/ButtonComponent.jsx
@@ -3,10 +3,20 @@ import './ButtonComponent.css';
 
 const ButtonComponent = (props) => {
     //States for selected symbol for re-rendering
-    const [selectedSymbol, setSelectedSymbol] = useState("");
+    const [selectedSymbol, setSelectedSymbol] = useState(window.localStorage.getItem("symbol"));
+    //Variable for selected/unselected
+    let selection;
+
+    //Checks if current selected stock symbol is same as button (including first time user which is null)
+    if(props.symbol === selectedSymbol){
+        selection = "ticker selected";
+    } else {
+        selection = "ticker unselected";
+    }
+
     return (
         <div className="button-div">
-            <button className="ticker unselected">{props.symbol}</button>
+            <button className={selection}>{props.symbol}</button>
         </div>
     )
 }
