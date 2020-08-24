a2cc838499b4f2d8191da9fcbc983f028731d20c
travisenquiry <travisenquiry@gmail.com>
2020-08-24 16:48:10 +0800

    Changed button component css to selected/unselected

--- src/components/Stock/Ticker/Button/ButtonComponent.jsx
+++ src/components/Stock/Ticker/Button/ButtonComponent.jsx
@@ -2,10 +2,11 @@ import React, { useEffect } from 'react';
 import './ButtonComponent.css';
 
 const ButtonComponent = () => {
+    //
 
     return (
         <div className="button-div">
-            <button className="ticker">MSFT</button>
+            <button className="ticker unselected">MSFT</button>
         </div>
     )
 }
